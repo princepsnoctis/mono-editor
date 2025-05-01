b4484808fe9393b001d266e2d579f73d453a7d24
princepsnoctis <noctisprinceps@gmail.com>
2025-05-01 20:56:59 +0200

    Fix minor formatting in App component
    Removed unnecessary imports and corrected inconsistent spacing in the heading. These changes improve code cleanliness and readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
   return (
-    <h1> Mono Editor</h1>
+      <h1>Mono Editor</h1>
   )
 }
 
